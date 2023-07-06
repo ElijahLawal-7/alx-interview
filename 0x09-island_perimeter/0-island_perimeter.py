346c6a480421f701610607691c933a98650e56de
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2023-07-05 21:47:59 -0700

    added

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""
-Island Perimeter
-"""
+''' island perimeter module '''
+
 
 def island_perimeter(grid):
-    """
-     returns the perimeter of the island described in grid
-    :param grid:
-    :return:
-    """
-    area = 0
-    for row in grid + list(map(list, zip(*grid))):
-        for i1, i2 in zip([0] + row, row + [0]):
-            area += int(i1 != i2)
-    return area
+    ''' calculates perimeter of an island '''
+
+    row_count = len(grid)
+    col_count = len(grid[0])
+
+    p = 0
+    for i in range(row_count):
+        for j in range(col_count):
+            if grid[i][j]:
+                u = (not i) or grid[i - 1][j] == 0
+                d = (i + 1 >= row_count) or grid[i + 1][j] == 0
+                l = (not j) or grid[i][j - 1] == 0
+                r = (j + 1 >= col_count) or grid[i][j + 1] == 0
+                p += u + d + l + r
+    return p
